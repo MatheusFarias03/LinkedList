07dde601a236ef9365a2c60c9a8e35173a773bb5
Matheus Farias de Oliveira Matsumoto <83461020+MatheusFarias03@users.noreply.github.com>
2022-05-06 13:24:52 -0300

    Mudei de stack para class

--- LinkedList.h
+++ LinkedList.h
@@ -1,8 +1,8 @@
 /*	Leonardo Pinheiro de Souza - 32127391
 	Matheus Farias de Oliveira Matsumoto - 32138271
 
-	Código em Inglês
-	Comentários em Portugês-BR 
+	CÃ³digo em InglÃªs
+	ComentÃ¡rios em PortugÃªs-BR 
 */
 
 #ifndef __H_LINKED_LIST__
@@ -10,50 +10,53 @@
 
 #include <iostream>
 
-template <typename T>
-struct Node {
-	T data;
-	Node* next;
+class Node {
+public:
+	Node();
+	~Node();
+
+	void DestroyNode();
+
+	void SetData(int data);
+	int GetData();
+
+	void SetNext(Node& value);
+	Node* GetNext();
+
+private:
+	int data;
+	Node* next = nullptr; 
 };
 
-struct LinkedList {
+
+class LinkedList {
+public:
+	LinkedList();
+	~LinkedList();
+
+	void Destroy();
+
+	void Insert(int elem);
+	void Append(int elem);
+
+	Node RemoveHead();
+	Node RemoveTail();
+	Node RemoveNode(int elem);
+	
+	Node GetHead();
+	Node GetTail();
+	Node GetNode(int elem);
+
+private:
 	int count;
-	struct Node* head;
-	struct Node* tail;
+	Node* head = nullptr;
+	Node* tail = nullptr;
+
 };
-
-// Operações de criação
-LinkedList Create();
-template <typename T> struct Node CreateNode(T data, Node& next); // Por que só aqui deve ser escrito o "template <typename T> struct Node" e nos outros não?
-
-// Operações para destruir
-void Destroy(LinkedList& linkedList);
-void DestroyNode(Node& node);
-
-// Operações para adicionar
-template <typename T> void AddFront(LinkedList& linkedList, T elem);
-template <typename T> void AddBack(LinkedList& linkedList, T elem);
-
-// Operações para remover
-Node RemoveFront(LinkedList& linkedList);
-Node RemoveBack(LinkedList& linkedList);
-template <typename T> Node RemoveNode(LinkedList& linkedList, T elem);
-
-// Operações para retornar uma referência do nó
-Node GetFront(LinkedList& linkedList);
-Node GetBack(LinkedList& linkedList);
-template <typename T> Node GetNode(LinkedList& linkedList, T elem);
-
-// Demais operações
-int Count(LinkedList& linkedList);
-bool IsEmpty(LinkedList& linkedList);
-void Clear(LinkedList& linkedList);
-
-
 
 #endif // !__H_LINKED_LIST__
 
-/* Referências: https://stackoverflow.com/questions/495021/why-can-templates-only-be-implemented-in-the-header-file
+/* ReferÃªncias: https://stackoverflow.com/questions/495021/why-can-templates-only-be-implemented-in-the-header-file
 				https://stackoverflow.com/questions/30430044/using-a-struct-that-is-defined-in-another-header
 				https://www.geeksforgeeks.org/generics-in-c/
 */
